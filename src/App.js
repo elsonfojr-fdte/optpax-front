dccf8878f4918e98200fa39b716d6ec54bd3932e
eJunior08 <elsonfojr@gmail.com>
2020-10-06 14:46:44 -0300

    Exibição do layout do cockpit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+/* Pages */
+import Layout from './pages/Layout';
+
+/* Styles */
+import GlobalStyles from './styles/GlobalStyles';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout />
+      <GlobalStyles />
+    </>
   );
-}
+};
 
 export default App;
