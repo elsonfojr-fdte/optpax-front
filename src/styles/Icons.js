64fed980012ce022431bde17fb7cd087952599c4
eJunior08 <elsonfojr@gmail.com>
2020-10-06 14:41:27 -0300

    Arquivo para reunir os icones criado

---
+++ src/styles/Icons.js
@@ -0,0 +1,9 @@
+export { DirectionsBus } from '@styled-icons/material';
+
+export { Aircraft } from '@styled-icons/entypo';
+
+export { Close, ExternalLink } from '@styled-icons/evaicons-solid';
+
+export { Cogs, History, PowerOff, ThList, Users } from '@styled-icons/fa-solid';
+
+export { Graph } from '@styled-icons/octicons';
