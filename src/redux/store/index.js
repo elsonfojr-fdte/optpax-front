75aa3908a0f512bad2dc5483124a1bd78687119b
eJunior08 <elsonfojr@gmail.com>
2020-10-07 17:44:30 -0300

    Configurando o store para usar configureStore

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,5 +1,6 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import { Reducers } from '../reducers';
 
-export const Store = createStore(Reducers, applyMiddleware(thunkMiddleware));
+export const Store = configureStore({
+  reducer: Reducers,
+});
