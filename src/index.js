f78bbc1d937a5380411420f25f42d395836126cb
eJunior08 <elsonfojr@gmail.com>
2020-10-08 08:03:34 -0300

    Import atualizado para novo local da store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Store } from './redux/store';
+import { Store } from './redux';
 
 import App from './App';
 
