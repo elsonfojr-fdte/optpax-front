50eee3b634b3f01b111b46c3a6a6c824c1785791
eJunior08 <elsonfojr@gmail.com>
2020-10-08 08:02:36 -0300

    Store movida para pasta redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { Reducers } from '../reducers';
+import { Reducers } from './reducers';
 
 export const Store = configureStore({
   reducer: Reducers,
